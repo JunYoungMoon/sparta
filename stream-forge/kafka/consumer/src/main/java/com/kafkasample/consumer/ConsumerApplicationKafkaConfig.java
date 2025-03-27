f794d606eae88db9945e154dc389ecb2b28c1a22
moon <ko5642027@naver.com>
2025-03-27 13:32:04 +0900

    대규모 AI 시스템 프로젝트 프로젝트 추가

--- stream-forge/kafka/consumer/src/main/java/com/kafkasample/consumer/ConsumerApplicationKafkaConfig.java
+++ stream-forge/kafka/consumer/src/main/java/com/kafkasample/consumer/ConsumerApplicationKafkaConfig.java
@@ -1,6 +1,8 @@
 package com.kafkasample.consumer;
 
+import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.config.SaslConfigs;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +19,8 @@ import java.util.Map;
 @Configuration // Spring 설정 클래스로 선언하는 어노테이션입니다.
 public class ConsumerApplicationKafkaConfig {
 
+
+
     // Kafka 컨슈머 팩토리를 생성하는 빈을 정의합니다.
     // ConsumerFactory는 Kafka 컨슈머 인스턴스를 생성하는 데 사용됩니다.
     // 각 컨슈머는 이 팩토리를 통해 생성된 설정을 기반으로 작동합니다.
@@ -25,12 +29,20 @@ public class ConsumerApplicationKafkaConfig {
         // 컨슈머 팩토리 설정을 위한 맵을 생성합니다.
         Map<String, Object> configProps = new HashMap<>();
         // Kafka 브로커의 주소를 설정합니다.
-        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.0.16:9092");
+        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "39.112.24.48:9092");
         // 메시지 키의 디시리얼라이저 클래스를 설정합니다.
         configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         // 메시지 값의 디시리얼라이저 클래스를 설정합니다.
         configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         // 설정된 프로퍼티로 DefaultKafkaConsumerFactory를 생성하여 반환합니다.
+
+        // SASL 인증 관련 설정 추가
+        configProps.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_PLAINTEXT");
+        configProps.put(SaslConfigs.SASL_MECHANISM, "PLAIN");
+        configProps.put(SaslConfigs.SASL_JAAS_CONFIG,
+                "org.apache.kafka.common.security.plain.PlainLoginModule required " +
+                        "username=\"default\" password=\"4yeargap!@34\";");
+
         return new DefaultKafkaConsumerFactory<>(configProps);
     }
 
